2dcfb3ef22b70514fe9f60dd859c0f8dad3a63f1
buivanxung <xungbv.uit@gmail.com>
2019-05-23 19:53:52 +0700

    update

--- server.js
+++ server.js
@@ -3,11 +3,10 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var allData = {'D0':0,'D1':0,'D2':0,'D3':0,'D4':0,'D5':0,'D6':0,'D7':0,'D8':0,'D9':0,'D10':0,'D11':0};
+
 app.use(express.static(__dirname + '/'));
 
-var statusD1,statusD2;
-var d_data;
-statusD1 = false; statusD2=false;
 var clientId = 'mqttjs_' + Math.random().toString(16).substr(2, 8)
 var client =  require('mqtt').connect('mqtt://34.74.202.175:1883', {
   keepalive: 10,
@@ -30,7 +29,6 @@ io.on('connection', function (socket) {
   client.on('connect', function () {
        console.log('connected:' + clientId)
   });
-
   client.on('error', function (err) {
        console.log(err)
        client.end()
@@ -39,8 +37,8 @@ io.on('connection', function (socket) {
   client.on('message', function (topic, message) {
     d_data = message.toString();
     socket.emit('sending_json_data', d_data);
-    var dStatus = message.toString().split(";");
-    var d1status = dStatus[0];
+    var dStatus = message.toString();
+    var dPin = dStatus[0];
     var d2status = dStatus[1];
     if (d1status[3] =='1') {
       statusD1 = true;
@@ -57,23 +55,10 @@ io.on('connection', function (socket) {
        console.log(clientId + ' disconnected')
   });
   socket.on('respond_command', function(data) {
-    if (data == "D1ON") {
-        client.publish('/device1/command', 'D0:1 ');
-        statusD1 = true;
-        console.log("T");
-      }
-    if (data == "D1OFF"){
-        client.publish('/device1/command', 'D0:0 ');
-        statusD1 = false;
+    var raw = data.toString().split(':');
+    if(allData.hasOwnProperty(raw[0])){
+      allData[raw[0]] = 1;
     }
-    if (data == "D2ON") {
-        client.publish('/device1/command', 'D1:1 ');
-        statusD2 = true;
-      }
-    if (data == "D2OFF"){
-        client.publish('/device1/command', 'D1:0 ');
-        statusD2 = false;
-      }
   })
 })
 http.listen(8448, function () {
