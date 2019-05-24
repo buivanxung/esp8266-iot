286a9cc7a8202a29f406a3edc1c53b376c3e2ac0
buivanxung <xungbv.uit@gmail.com>
2019-05-24 23:06:51 +0700

    ip

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 var allData = {'D0':0,'D1':0,'D2':0,'D3':0,'D4':0,'D5':0,'D6':0,'D7':0,'D8':0,'D9':0,'D10':0,'D11':0,
-'D12':0,'D13':0,'D14':0,'D15':0,'D16':0,'D17':0,'D18':0,'D19':0,'D20':0,'D21':0,'D22':0,'D23':0};
-var numberButton;
+'D12':0,'D13':0,'D14':0,'D15':0,'D16':0,'D17':0,'D18':0,'D19':0,'D20':0,'D21':0,'D22':0,'D23':0,
+'D24':0,'D25':0,'D26':0,'D27':0,'D28':0,'D29':0,'D30':0,'D31':0,'D32':0,'D33':0,'D34':0,'D35':0,
+'D36':0,'D37':0,'D38':0,'D39':0,'D40':0,'D41':0,'D42':0,'D43':0,'D44':0,'D45':0,'D46':0,'D47':0,
+'D48':0,'D49':0};
 
 app.use(express.static(__dirname + '/'));
 
@@ -29,7 +31,8 @@ app.get('/', function(req, res){
 io.on('connection', function (socket) {
   console.log("New connection");
   client.on('connect', function () {
-       console.log('connected:' + clientId)
+       console.log('connected:' + clientId);
+       socket.emit('alldata', allData);
   });
   client.on('error', function (err) {
        console.log(err)
